89c08bec7d6891603c1719d789413e5a4b6a49bc
LÊ NGỌC VI <you@example.com>
2022-03-04 23:23:34 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const setupContactRouters = require("./app/routes/contact.routes");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+    res.json({ message: "Welcome to contac book application." });
 });
+
+setupContactRouters(app);
 
 module.exports = app;
---
+++ app/routes/contact.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const ContactController = require("../controllers/contact.controller");
+
+module.exports = app => {
+    const router = express.Router();
+
+    router.get("/", contacts.findAll);
+
+    router.post("/", contacts.create);
+
+    router.delete("/", contacts.deleteAll);
+
+    router.get("/favorite", contacts.findAllFavorites);
+    
+    router.get("/:id", contacts.findOne);
+
+    router.put("/:id", contacts.update);
+
+    router.delete(":/id", contacts.delete);
+
+    app.use("/api/contacts", router);
+
+};
