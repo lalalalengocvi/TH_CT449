70fbab6543f737319a279be51bda8a25f1585553
LÊ NGỌC VI <you@example.com>
2022-03-04 23:30:35 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+
 const express = require("express");
 const cors = require("cors");
-const app = express();
-
-const setupContactRouters = require("./app/routes/contact.routes");
+const setupContactRouter = require("./app/routes/contact.routes")
+const { BadRequestError, errorHandler } = require("./app/errors");
+const app =express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) =>{
+    res.json({message: "Welcom to contact book application"});
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contac book application." });
 });
 
-setupContactRouters(app);
+setupContactRouter(app);
+
+app.use((req, res,next) =>{
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) =>{
+    errorHandler.handleError(error, res);
+});
 
 module.exports = app;
